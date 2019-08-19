c4d207940274b8d0f9d319c6690ecd7c7c912b26
thomaspuppe <info@thomaspuppe.de>
2019-08-19 16:42:09 +0200

    Store data also in our new Graphite

--- src/utils/sendToGraphite.js
+++ src/utils/sendToGraphite.js
@@ -7,11 +7,21 @@
 const graphite = require('graphite')
 
 module.exports = function (metrics) {
-  const client = graphite.createClient('plaintext://sitespeed.zeit.de:2003/')
-  client.write(metrics, function (error) {
+  const clientOld = graphite.createClient('plaintext://sitespeed.zeit.de:2003/')
+  clientOld.write(metrics, function (error) {
     if (typeof error !== 'undefined') {
       console.error(error)
     }
   })
-  client.end()
+  clientOld.end()
+
+  const clientNew = graphite.createClient('plaintext://10.110.16.26:2003/')
+  clientNew.write(metrics, function (error) {
+    if (typeof error !== 'undefined') {
+      console.error(error)
+    }
+  })
+  clientNew.end()
+
+
 }
