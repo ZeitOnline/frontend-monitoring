18f3885e1cc6619e1380da4e9a449ebc8f75f0cf
thomaspuppe <info@thomaspuppe.de>
2019-10-08 13:34:19 +0200

    MAINT: Do no longer send data to old Graphite

--- src/utils/sendToGraphite.js
+++ src/utils/sendToGraphite.js
@@ -7,13 +7,6 @@
 const graphite = require('graphite')
 
 module.exports = function (metrics) {
-  const clientOld = graphite.createClient('plaintext://sitespeed.zeit.de:2003/')
-  clientOld.write(metrics, function (error) {
-    if (typeof error !== 'undefined') {
-      console.error(error)
-    }
-  })
-  clientOld.end()
 
   const clientNew = graphite.createClient('plaintext://10.110.16.26:2003/')
   clientNew.write(metrics, function (error) {
@@ -22,6 +15,5 @@ module.exports = function (metrics) {
     }
   })
   clientNew.end()
-
 
 }
