349291109b754cb82ecd40606534f2adc94cfa19
thomaspuppe <info@thomaspuppe.de>
2020-02-11 10:04:19 +0100

    get CSSstats for all ZON verticals

--- src/checks/cssstats/index.js
+++ src/checks/cssstats/index.js
@@ -2,9 +2,14 @@ const cssstats = require('./check')
 const URLS = require('../../config/urls')
 
 const site = 'zeit-de'
-for (let type in URLS[site]) {
-  const url = URLS[site][type]
-  cssstats.run(site, type, url).then(() => {
-    console.log(`Finished cssstats for ${url}`)
-  })
+for (let site in URLS) {
+  if (site.indexOf('zeit-') !== 0) {
+    continue;
+  }
+  for (let type in URLS[site]) {
+    const url = URLS[site][type]
+    cssstats.run(site, type, url).then(() => {
+      console.log(`Finished cssstats for ${url}`)
+    })
+  }
 }
