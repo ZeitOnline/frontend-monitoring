0e0d85d3d8e2317200665485a699afe52f47d5dd
Hennes Römmer <hroemmer@googlemail.com>
2020-07-24 15:28:19 +0200

    FIX: Use monitoring useragent in cssstats, too

--- src/checks/cssstats/check.js
+++ src/checks/cssstats/check.js
@@ -9,35 +9,38 @@ const statsFilter = require('./filters/stats')
 exports = module.exports = {}
 
 exports.run = function run (siteName, siteType, url) {
-  return getCss(url)
-    .then(function (response) {
-      const css = getCompleteCss(response.links)
+  return getCss(url, {
+    headers: {
+      'User-Agent': 'ZONFrontendMonitoring'
+    }
+  }).then(function (response) {
+    const css = getCompleteCss(response.links)
 
-      const results = cssStats(css, {
-        mediaQueries: false,
-        importantDeclarations: true
-      })
+    const results = cssStats(css, {
+      mediaQueries: false,
+      importantDeclarations: true
+    })
 
-      const stats = statsFilter(results)
+    const stats = statsFilter(results)
 
-      const metrics = {
-        cssstats: {
-          [siteName]: {
-            [siteType]: {
-              stats
-            }
+    const metrics = {
+      cssstats: {
+        [siteName]: {
+          [siteType]: {
+            stats
           }
         }
       }
+    }
 
-      sendToGraphite(metrics)
-      // TODO: zentrales console.log, wenn Parameter --verbose gesetzt wurde
-      // console.log(metrics)
+    sendToGraphite(metrics)
+    // TODO: zentrales console.log, wenn Parameter --verbose gesetzt wurde
+    // console.log(metrics)
 
-      saveRawData(results, `${siteName}_${siteType}_cssstats`)
+    saveRawData(results, `${siteName}_${siteType}_cssstats`)
 
-      return metrics
-    })
+    return metrics
+  })
     .catch(function (error) {
       console.error(error)
     })
