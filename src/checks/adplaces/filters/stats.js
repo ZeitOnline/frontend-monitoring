bac241f9c7be721793f1a9ded2cd71e64318c7a9
thomaspuppe <info@thomaspuppe.de>
2020-09-15 11:37:46 +0200

    Fix check for adtiles

--- src/checks/adplaces/filters/stats.js
+++ src/checks/adplaces/filters/stats.js
@@ -15,7 +15,7 @@ module.exports = dom => {
 
   const adtilesToCheck = [
   'ad-desktop-1', 'ad-desktop-12', 'ad-desktop-13', 'ad-desktop-16', 'ad-desktop-2', 'ad-desktop-3', 'ad-desktop-4', 'ad-desktop-41', 'ad-desktop-42', 'ad-desktop-43', 'ad-desktop-5', 'ad-desktop-51', 'ad-desktop-8', 'ad-desktop-81', 'ad-desktop-9', 'ad-desktop-99',
-  'ad-mobile-1', 'ad-mobile-2', 'ad-mobile-3', 'ad-mobile-4', 'ad-mobile-41', 'ad-mobile-42', 'ad-mobile-43', 'ad-mobile-8', 'ad-mobile-99'
+  'ad-mobile-1', 'ad-mobile-16', 'ad-mobile-2', 'ad-mobile-3', 'ad-mobile-4', 'ad-mobile-41', 'ad-mobile-42', 'ad-mobile-43', 'ad-mobile-8', 'ad-mobile-99'
   ]
 
   for (const ad of adtilesToCheck) {
