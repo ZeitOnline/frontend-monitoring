6f86cf5a742bdaebf52f0a4e96aee32046bf6dd6
hennnnes <hroemmer@googlemail.com>
2021-08-16 16:39:15 +0200

    MAINT: Add example staging url

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,42 +1,45 @@
 const URLS = {
   'spiegel-de': {
-    'homepage': 'http://www.spiegel.de/',
-    'article': 'http://www.spiegel.de/politik/deutschland/steinmeier-empfaengt-schulz-merkels-hoffnungstraeger-a-1179786.html',
-    'ressort': 'http://www.spiegel.de/politik/'
+    homepage: 'http://www.spiegel.de/',
+    article: 'http://www.spiegel.de/politik/deutschland/steinmeier-empfaengt-schulz-merkels-hoffnungstraeger-a-1179786.html',
+    ressort: 'http://www.spiegel.de/politik/'
   },
   'welt-de': {
-    'homepage': 'https://www.welt.de/',
-    'article': 'https://www.welt.de/politik/deutschland/article170905545/Doppelspitze-der-Alphatiere.html',
-    'ressort': 'https://www.welt.de/politik/'
+    homepage: 'https://www.welt.de/',
+    article: 'https://www.welt.de/politik/deutschland/article170905545/Doppelspitze-der-Alphatiere.html',
+    ressort: 'https://www.welt.de/politik/'
   },
   'zeit-de': {
-    'homepage': 'https://www.zeit.de/index',
-    'article': 'https://www.zeit.de/politik/ausland/2018-04/james-comey-donald-trump-fbi-ermittlungen-russlandaffaere',
-    'ressort': 'https://www.zeit.de/politik/index'
+    homepage: 'https://www.zeit.de/index',
+    article: 'https://www.zeit.de/politik/ausland/2018-04/james-comey-donald-trump-fbi-ermittlungen-russlandaffaere',
+    ressort: 'https://www.zeit.de/politik/index'
   },
   'zeit-arbeit': {
-    'homepage': 'https://www.zeit.de/arbeit/index',
-    'article': 'https://www.zeit.de/arbeit/2019-05/eugh-eu-staaten-muessen-arbeitgeber-zur-zeiterfassung-verpflichten-kommentar',
-    'ressort': 'https://www.zeit.de/thema/selbstverwirklichung'
+    homepage: 'https://www.zeit.de/arbeit/index',
+    article: 'https://www.zeit.de/arbeit/2019-05/eugh-eu-staaten-muessen-arbeitgeber-zur-zeiterfassung-verpflichten-kommentar',
+    ressort: 'https://www.zeit.de/thema/selbstverwirklichung'
   },
   'zeit-campus': {
-    'homepage': 'https://www.zeit.de/campus/index',
-    'article': 'https://www.zeit.de/campus/2018/05/wohnungsmarkt-wohnen-mietpreise-tiny-house-wohnheim-helfen-pendeln',
-    'ressort': 'https://www.zeit.de/thema/liebe-und-sex'
+    homepage: 'https://www.zeit.de/campus/index',
+    article: 'https://www.zeit.de/campus/2018/05/wohnungsmarkt-wohnen-mietpreise-tiny-house-wohnheim-helfen-pendeln',
+    ressort: 'https://www.zeit.de/thema/liebe-und-sex'
   },
   'zeit-magazin': {
-    'homepage': 'https://www.zeit.de/zeit-magazin/index',
-    'article': 'https://www.zeit.de/zeit-magazin/leben/2019-05/apnoetauchen-freitauchen-anna-von-boetticher-extremsport-weltrekorde',
-    'ressort': 'https://www.zeit.de/zeit-magazin/leben/index'
+    homepage: 'https://www.zeit.de/zeit-magazin/index',
+    article: 'https://www.zeit.de/zeit-magazin/leben/2019-05/apnoetauchen-freitauchen-anna-von-boetticher-extremsport-weltrekorde',
+    ressort: 'https://www.zeit.de/zeit-magazin/leben/index'
   },
   'zeit-zett': {
-    'homepage': 'https://www.zeit.de/zett/index',
-    'article': 'https://www.zeit.de/zett/liebe-sex/2020-12/antimuslimischer-rassismus-pornografie-hijab-pornos-fetisch-sexismus',
-    'ressort': 'https://www.zeit.de/zett/index'
+    homepage: 'https://www.zeit.de/zett/index',
+    article: 'https://www.zeit.de/zett/liebe-sex/2020-12/antimuslimischer-rassismus-pornografie-hijab-pornos-fetisch-sexismus',
+    ressort: 'https://www.zeit.de/zett/index'
   },
   'zeit-pure': {
-    'homepage': 'https://www.zeit.de/zustimmung',
-    'article': 'https://www.zeit.de/impressum/index'
+    homepage: 'https://www.zeit.de/zustimmung',
+    article: 'https://www.zeit.de/impressum/index'
+  },
+  'zeit-staging': {
+    article: 'https://www.staging.zeit.de/test/test-hennes/embed-test'
   }
 }
 
